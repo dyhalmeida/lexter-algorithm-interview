f3273e6bb7bce9e0611e61d9a9a070ab700946c9
dyhalmeida <dyhalmeida@gmail.com>
2024-03-14 10:32:41 -0300

    feat: add http server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("running");
+import { createServer } from "http";
+
+import { TreeUsecase } from "./usecase/tree-usecase";
+import { TreeController } from "./controller/tree-controller";
+
+const treeUsecase = new TreeUsecase();
+const treeController = new TreeController(treeUsecase);
+
+const server = createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Lexter Algorithm Test Interview" }));
+  } else if (req.method === "POST" && req.url === "/tree") {
+    treeController.handler(req, res);
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "Not Found" }));
+  }
+});
+
+const PORT = process.env.PORT;
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}`);
+});
